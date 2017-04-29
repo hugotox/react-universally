6e133721f962729516646702a940180305f27c3f
hugotox <hpineda83@gmail.com>
2017-04-29 16:34:57 -0300

    Add sass and css modules

--- src/shared/components/DemoApp/Home/Home.js
+++ src/shared/components/DemoApp/Home/Home.js
@@ -3,13 +3,14 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { safeConfigGet } from '../../../utils/config';
+import styles from './Home.scss';
 
 function Home() {
   return (
     <article>
       <Helmet title="Home" />
 
-      <h2>{safeConfigGet(['welcomeMessage'])}</h2>
+      <h2 className={styles.test}>{safeConfigGet(['welcomeMessage'])}</h2>
 
       <p>
         This starter kit contains all the build tooling and configuration you
